16f2bad6a7b14dda41b97112def3e99c69a3b40e
zephyrfalcon <zephyrfalcon@gmail.com>
2010-05-07 16:59:51 -0400

    First file... *belch*

---
+++ s1.scm
@@ -0,0 +1,14 @@
+;; s1.scm
+
+(define (read-data port)
+  ...)
+
+(define (write-data port)
+  ...)
+
+(define (main args)
+  (print "Hello world!")
+  (print args))
+
+;; read data from stdin
+;; write data to stdout
